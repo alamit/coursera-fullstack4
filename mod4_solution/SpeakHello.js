7f7ae68c909abd9aefc3edfc88e767d696283877
alamit <adrien.lamit@effixis.ch>
2017-12-08 10:55:11 +0100

    assignment4 step3

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -1,8 +1,10 @@
 (function () {
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
-// var helloSpeaker =
+var helloSpeaker = {
+  speak: function (window) {
+
+  }
+};
+
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
   var speakWord = 'Hello';
