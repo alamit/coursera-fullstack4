9a138319c2183cc3de47d8da586ef63757b5ecb6
alamit <adrien.lamit@effixis.ch>
2017-12-08 10:59:28 +0100

    assignment4 step5-6

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -3,15 +3,10 @@
   var speakWord = 'Hello';
 
   var helloSpeaker = {
-    speak: function (window, name) {
+    speak: function (name) {
       console.log(speakWord);
     },
   };
 
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
-
-});
+  this.helloSpeaker = helloSpeaker;
+})();
