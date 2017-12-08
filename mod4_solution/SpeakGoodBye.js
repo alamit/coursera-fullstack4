f8d9034fcf40d33c0432372bc005748ef031e3a3
alamit <adrien.lamit@effixis.ch>
2017-12-08 11:01:08 +0100

    assignment4 step9

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -11,7 +11,6 @@
     },
   };
 
-  // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-  // 'byeSpeaker' on the global scope as well.
-  // xxxx.xxxx = byeSpeaker;
+  this.byeSpeaker = byeSpeaker;
+  
 })();
